9182bb0134661a6f41abeea76ec4770b6c8f430e
bilgeyucel <bilgeyucel96@gmail.com>
2023-02-22 10:29:22 +0100

    test if removing clone argument works

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf haystack-tutorials
-git clone --depth 1 https://github.com/deepset-ai/haystack-tutorials.git
+git clone https://github.com/deepset-ai/haystack-tutorials.git
 echo "Copying markdown files into ./content/tutorials..."
 cp ./haystack-tutorials/markdowns/* ./content/tutorials
 ls ./content/tutorials
