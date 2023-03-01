f5b2a6e0c2f20de40c05005354ab7902905a5398
bilgeyucel <bilgeyucel96@gmail.com>
2023-03-01 18:24:13 +0300

    change build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 rm -rf haystack-tutorials
-git clone --filter=tree:0 https://github.com/deepset-ai/haystack-tutorials.git
+git clone --depth 1 --branch implement-last-commit https://github.com/deepset-ai/haystack-tutorials.git 
+
+echo "Install requirements for haystack-tutorials..."
+pip install -r haystack-tutorials/requirements.txt
+echo "Generating markdown files into ./content/tutorials..."
+python haystack-tutorials/scripts/generate_markdowns.py --index haystack-tutorials/index.toml --notebooks all --output ./content/tutorials
 echo "Copying markdown files into ./content/tutorials..."
-cp ./haystack-tutorials/markdowns/* ./content/tutorials
 ls ./content/tutorials
 mkdir ./static/downloads
 echo "Copying notebook files into ./static/downloads..."
